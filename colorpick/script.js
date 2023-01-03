f998a09c6d7be60e564c43249f0126c2ef131ba4
Mohit030802 <mohitom2002@gmail.com>
2023-01-03 23:41:07 +0530

    day3

---
+++ colorpick/script.js
@@ -0,0 +1,52 @@
+ const colorPickerBtn=document.querySelector("#color-picker");
+ const colorList=document.querySelector(".all-colors");
+ const clearAll=document.querySelector(".clear-all");
+ const pickedColors=JSON.parse(localStorage.getItem("picked-colors")||"[]");
+ const copyColor=elem=>{
+navigator.clipboard.writeText(elem.dataset.color); 
+elem.innerText="copied";
+setTimeout(()=>elem.innerText=elem.dataset.color,1000);
+}
+ 
+ const showColors=()=>{
+    if(!pickedColors.length) return;
+    colorList.innerHTML=pickedColors.map(color=>`
+    <li class="color">
+                    <span class="rect" style="background:${color}; border:1px solid ${color=="fffff"?"#ccc":color}"></span>
+                    <span class="value">#${color}</span>
+                </li>
+    `).join("");
+
+    document.querySelector(".picked-colors").classList.remove("hide");
+    document.querySelectorAll(".color").forEach(li=>{
+        li.addEventListener("click",e=>copyColor(e.currentTarget.lastElementChild));
+    })
+    
+ }
+ showColors(); 
+
+ const activateEyeDropper=async()=>{
+    try{
+        const eyeDropper=new eyeDropper();
+        // const response=await eyeDropper.open();
+        const {sRGBHex}=await eyeDropper.open();
+        navigator,Clipboard.writeText(sRGBHex);
+        if(!pickedColors.includes(sRGBHex)){
+            pickedColors.push(sRGBHex);
+            // console.log(sRGBHex)
+            localStorage.setItem("picked-colors",JSON.stringify(pickedColors));
+            showColors();
+        }
+       
+    }
+    catch (error){
+        console.log(error)
+    }
+ } 
+ const clearAllColors=()=>{
+    pickedColors.length=0;
+    localStorage.setItem("picked-colors",JSON.stringify(pickedColors));
+    document.querySelector(".picked-colors").classList.remove("hide");
+ }
+colorPickerBtn.addEventListener("click",clearAllColors);
+clearAll.addEventListener("click",activateEyeDropper);
